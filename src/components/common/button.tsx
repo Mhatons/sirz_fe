c161288866e601adc6eab8c10e38ccb375a21c8d
Mhatons <mhatons@gmail.com>
2024-10-20 23:50:17 +0100

    updated changes

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -1,15 +1,16 @@
 
 interface Ibutton {
     name: string,
-    // onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+    onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 export default function Button(props: Ibutton) {
-    const { name } = props;
+    const { name, onClick } = props;
+
     return (
         <button
-            onClick={() => window.open('https://t.me/Portalearn_bot', '_blank')}
-            className=" bg-button border capitalize text-lg rounded-md border-white text-white py-3 px-7 font-extrabold">
+            onClick={onClick || (() => window.open('https://t.me/Portalearn_bot', '_blank'))}
+            className="bg-button border capitalize text-lg rounded-md border-white text-white py-3 px-7 font-extrabold">
             {name}
         </button>
-    )
+    );
 }
