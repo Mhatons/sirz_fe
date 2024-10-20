2ea03a201fc92ff0a8441f508c95b842fecbce28
Mhatons <mhatons@gmail.com>
2024-10-21 00:45:52 +0100

    updated cards

--- src/components/layout/card.tsx
+++ src/components/layout/card.tsx
@@ -11,8 +11,8 @@ export default function Card(props: ICard) {
         <div className="flex px-16 h-[200px] hover:transform hover:-translate-y-2 transition-transform duration-300 card_shadow cursor-pointer rounded-3xl p-6 items-center justify-center">
             <div className="flex items-center justify-center flex-col ">
                 <div className="text-[50px] text-[#06C6D1] font-extrabold">{icon}</div>
-                <header className="text-[25px] font-semibold">{title}</header>
-                <div className="text-center text-lg">{description}</div>
+                <header className="text-[23px] font-semibold whitespace-nowrap">{title}</header>
+                <div className="text-center text-[17px]">{description}</div>
             </div>
         </div>
     )
