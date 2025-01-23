b5247e10cbaaea188bdf8282c7d656b4a96e5d7c
Mhatons <mhatons@gmail.com>
2025-01-23 23:18:33 +0100

    updated

--- src/components/layout/animatedCard.tsx
+++ src/components/layout/animatedCard.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from "react";
+
 interface Idata {
     title: string,
     desc: string,
@@ -7,8 +9,34 @@ interface Idata {
 
 export default function AnimatedCard(props: Idata) {
     const { title, desc, index, image } = props;
+    const [isActive, setIsActive] = useState(false);
+    const animatedCardRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (animatedCardRef.current) {
+                const card = animatedCardRef.current;
+                const cardRect = card.getBoundingClientRect();
+                const windowHeight = window.innerHeight;
+
+                const isInMiddle =
+                    cardRect.top + cardRect.height / 2 > windowHeight / 3 &&
+                    cardRect.top + cardRect.height / 2 < (2 * windowHeight) / 3;
+
+                setIsActive(isInMiddle)
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        handleScroll();
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
+
     return (
-        <div className=" relative animatedcardcontainer overflow-y-hidden mb-4 rounded-3xl h-[350px]">
+        <div
+            ref={animatedCardRef}
+            className={`relative animatedcardcontainer overflow-y-hidden mb-4 rounded-3xl sm:h-[350px] h-[420px] ${isActive ? 'in-view' : ''}`}
+        >
             <img src={image} alt="" className=' w-full object-cover h-full rounded-3xl' />
             <div className='bg-[#F9FCFF] dark:bg-background_dark dark:text-background_light text-[20px]  text-background_dark animatedcardtext absolute bottom-0 p-2 rounded-3xl rounded-br-none w-[90%] hover:text-white'>
                 <p className=" text-zinc-400">{index}</p>
