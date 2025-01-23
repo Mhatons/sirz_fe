bdbce3579efc403d94a36887b9b5edcf851e21dc
Mhatons <mhatons@gmail.com>
2025-01-23 13:58:52 +0100

    updated

--- src/pages/home/components/ourValues.tsx
+++ src/pages/home/components/ourValues.tsx
@@ -64,16 +64,16 @@ export default function CoreValues() {
         <div className="  bg-background_light dark:bg-background_dark dark:text-white pt-12 pb-8">
             <header className=" text-center pb-4 sm:w-[60%] w-[90%] m-auto">
                 <h4 className=' text-[30px] pb-2'> We Deliver on our Word</h4>
-                <div className='text-lg'>Our expert designers bring a wealth of experience from transforming spaces for a diverse range of residential, commercial, and corporate clients.</div>
-                <div>Check our core values below</div>
+                <div className='text-lg text-zinc-500'>Our expert designers bring a wealth of experience from transforming spaces for a diverse range of residential, commercial, and corporate clients.</div>
+                <div className=' text-primary text-[20px] pt-3'>Check our core values below</div>
             </header>
-            <ul className=" p-4 text-[12px] font-comfortaa ">
+            <ul className=" p-4 text-[14px] font-comfortaa ">
                 <Slider {...settings}>
                     {
                         coreValues.map((value, index) => (
                             <div className=' px-2'>
-                                <li key={index} className=" shadow-inner rounded-lg px-4 py-2 h-20 border">
-                                    <header className=" text-primary_dark text-sm font-semibold  pb-1 rounded-sm">{value.title}</header>
+                                <li key={index} className=" shadow-inner rounded-lg px-4 py-2 h-24 border">
+                                    <header className=" text-primary_dark text-[20px] font-semibold  pb-1 rounded-sm">{value.title}</header>
                                     <div>{value.description}</div>
                                 </li>
                             </div>
